77e3e70e612685ac72c991cafa2006c1bc518476
Wesley Tokoi <tokoiwesley@gmail.com>
2026-01-09 13:56:28 +0300

    feat: incorporte custom fonts into the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        // appBar: AppBar(backgroundColor: Colors.teal, title: Text('Demo')),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.lightGreen,
+                backgroundImage: Image(
+                  image: AssetImage('assets/images/wesley.jpeg'),
+                ).image,
+              ),
+              Text(
+                'Wesley Tokoi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 32.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
